ad18e0783aa680a32bff919f90a429e2716d28c3
ye1n <uyeinyein@gmail.com>
2021-09-03 20:49:05 +0900

    PHP 방과후 수업

--- s0902/lib.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function dump($value){
-    echo "<div class ='dump' style = 'display:inline-block; width:50%;' >";
-    echo "<pre>";
-    var_dump($value);
-    echo "</pre>";
-    echo "</div>";
-}
